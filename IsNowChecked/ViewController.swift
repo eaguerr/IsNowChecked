d47ee5fd084b2bec6a322bb52fbb84af6db33f12
Eduard Guerra <eaguerra@students.tulsatech.edu>
2019-11-12 09:01:28 -0600

    Added Label

---
+++ IsNowChecked/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IsNowChecked
+//
+//  Created by A.M. Student on 11/12/19.
+//  Copyright © 2019 A.M. Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
